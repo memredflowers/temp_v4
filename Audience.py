775822ce582b7657f123ee2d38521567992bf17b
Makin <157699353+memredflowers@users.noreply.github.com>
2024-01-26 18:54:49 +0100

    Create Audience.py

---
+++ Audience.py
@@ -0,0 +1,49 @@
+# python setup.py bdist_wheel upload
+
+from setuptools import setup
+import vrequest
+ 
+setup(
+    name = "vrequest",
+    version = vrequest.__version__,
+    keywords = "vrequest",
+    author = "cilame",
+    author_email = "opaquism@hotmail.com",
+    url="https://github.com/cilame/vrequest",
+    license = "MIT",
+    description = "",
+    long_description = '',
+    long_description_content_type="text/markdown",
+    classifiers = [
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+    ],
+
+    packages = [
+        "vrequest",
+    ],
+    package_data ={
+        "vrequest":[
+            'template/*',
+            'template/v/*',
+            'template/v/spiders/*',
+            'ico.ico',
+            'astjs.zip',
+            'main.js',
+            'ast_hook_inject.js',
+        ]
+    },
+    python_requires=">=3.0",
+    install_requires=[
+       'requests',
+       'lxml',
+    ],
+    entry_points={
+        'gui_scripts': [
+            'vv = vrequest.main:execute',
+            'vrequest = vrequest.main:execute',
+            'ee = vrequest.main:algo',
+        ]
+    },
+)
